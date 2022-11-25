cf2f412547bbbedcd5e8c3758cc9e6337a4598ef
Roman <romanbuznyk@gmail.com>
2022-11-25 12:05:40 +0200

    HW6

--- Module1/HW6/HW6_block3_task2.py
+++ Module1/HW6/HW6_block3_task2.py
@@ -1,8 +1,31 @@
 import random
 
-numbersbank = list(range(1, 10000))
-dice = random.choice(numbersbank)
-for i in numbersbank:
-    if dice == 1:
-        print(f'1 occured "i" times')
+side1 = 0 #тут змінні для кожної сторони
+side2 = 0
+side3 = 0
+side4 = 0
+side5 = 0
+side6 = 0
+for i in range(1, 10000): #далі буде виконуватися цикл для кожної і 10000 раз
+    dice = random.randint(1, 7)# зміннв для кубика яка дає цифру від 1 до 6 
+    if dice == 1: # умова якщо випало 1 
+        side1 += 1  # лічильник 
+    elif dice == 2:
+        side2 += 1
+    elif dice == 3:
+        side3 += 1
+    elif dice == 4:
+        side4 += 1
+    elif dice == 5:
+        side5 += 1
+    elif dice == 6:
+        side6 += 1
+   
     
+    print('Side 1 appeard', side1, 'times  '\
+    'Side 2 appeard', side2, 'times  '\
+    'Side 3 appeard', side3, 'times  '\
+    'Side 4 appeard', side4, 'times  '\
+    'Side 5 appeard', side5, 'times  '\
+    'Side 6 appeard', side6, 'times')
+
