de34f523142de1047780b282d2dedcea80fc69cf
Roman <romanbuznyk@gmail.com>
2022-12-03 12:30:37 +0200

    module2

---
+++ Module2/try_faker.py
@@ -0,0 +1,8 @@
+from faker import Faker
+from try_faker import create_user
+
+
+fake = Faker()
+def create_user(name: str, password: str):
+    print(fake.name())
+print(create_user('name', 'pass'))
